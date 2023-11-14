fb9a028a07a0115f69b3b53c99f7c8ae77c2c6f2
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-14 20:57:25 +0600

    reload after  delete complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useLoaderData } from "react-router-dom";
 import "./App.css";
 import CoffeeCard from "./components/CoffeeCard";
+import { useState } from "react";
 
 function App() {
-	const coffees = useLoaderData();
+	const loadedcoffees = useLoaderData();
+	const [coffees, setCoffees] = useState(loadedcoffees);
 
 	return (
 		<div className="m-20">
@@ -15,7 +17,11 @@ function App() {
 			<div className="grid md:grid-cols-2 gap-4">
 				{coffees.map((coffee) => (
 					// console.log(coffee)
-					<CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>
+					<CoffeeCard
+						key={coffee._id}
+						coffee={coffee}
+						coffees={coffees}
+						setCoffees={setCoffees}></CoffeeCard>
 				))}
 			</div>
 		</div>
